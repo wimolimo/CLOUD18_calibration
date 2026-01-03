40002edcb2e1d7a5eb9ac4fd9c02156bf6b1c93f
Clea Ruth <clea.ruth@student.uibk.ac.at>
2026-01-03 20:44:09 +0100

    minor changes in structure

--- src/calibrate_traces.jl
+++ src/calibrate_traces.jl
@@ -1,42 +1,65 @@
+#main TO Do: find out what to do about humparams and cloudhum, where to get it from.
 module Calibrate_Traces
 
-#export ....
+# export calibrate_traces_main(CalibrationConfig)
 
 #using ... from ...
 using HDF5
 using CSV
 using DataFrames
+using Dates
+using Plots
+using DelimitedFiles
+#import Statistics
 
+#check !
 using ToFTracer2
 import TOFTracer2.CalibrationFunctions as CalF
 import TOFTracer2.ImportFunctions as ImpF
+import TOFTracer2.InterpolationFunctions as IntpF
+import TOFTracer2.ExportFunctions as ExpF
+import TOFTracer2.ResultFileFunctions
+import TOFTracer2.PlotFunctions
+import TOFTracer2.MasslistFunctions
+import TOFTracer2.massLibrary
 
 ################################
 # define relative filepaths of used data
 ################################
 
+struct CalibrationConfig
+    dir_licor_data::String
+    humcalibfile::String
+    drycalibsfile::String
+    resultfp::String
+    resultfiles::Vector{String}
+    ionization::String
+    primaryionslist #::Vector{Float64}
+    refMass #::Float64
+    refName::String
+    exportTraces::Bool
+    HeaderForExportDict::Dict{String,Any}
+end
+
 dir_CLOUD18 = joinpath(@__DIR__, "..", "..", "..")
-dir_data = joinpath(dir_CLOUD18, "CLOUD18_data")
-dir_calib_data = joinpath(dir_data, "Calibration")
-dir_licor_data = joinpath(dir_data, "Licor")
+dir_calib_data = joinpath(dir_CLOUD18, "CLOUD18_data", "Calibration")
+dir_licor_data = joinpath(dir_CLOUD18, "CLOUD18_data", "Licor")
 
 #dry calibration file
 drycalibsfile = joinpath(dir_calib_data, "2025-11-25 08h28m48_1ppb_std_brown.h5") #FILENAME ANPASSEN!!! WIE SIEHT DIESE FILE AUS???
 
 #humidity dependent calibration file
-dir_humidcalib = joinpath(dir_calib_data, "Humidity-dependent_std", "results")
-humidcalibsfile = joinpath(dir_humidcalib, "_result.hdf5") #STIMMT FILENAME??? WAS FÜR EINE FILE SOLL DAS SEIN; EHER txt ???
+humcalibfile = joinpath(dir_calib_data, "Humidity-dependent_std", "results", "_result.hdf5") #humcalibfp; should be txt!!!! #from humidity dependence calibration script?
+
+#file to be calibrated at once with same mass list
+resultfp = joinpath(dir_calib_data, "Test") #change result filepath to data that is analyzed, results of this script are also saved here
+resultfiles = ["$(resultfp)/results/_result.hdf5"] #adjust filename, can add multiple files #["$(resultfp)part1/results/_result.hdf5","$(resultfp)part2/results/_result.hdf5"]
 
 ionization = "NH4+" # "NH4+", "H+"...
 primaryionslist = [] # leave empty -> default: adding all possible water and ammonium clusters
 
-
 refMass = massLibrary.HEXANONE_nh4[1] #mass of hexanone + NH4+ from julia package manualMassLibrary.jl
 refName = TOFTracer2.MasslistFunctions.sumFormulaStringFromCompositionArray(massLibrary.HEXANONE_nh4[4]; ion = "")
-
-#file to be calibrated at once with same mass list
-dir_results_to_calibrate = joinpath(dir_calib_data, "Test")
-resultsfiles_to_calibrate = ["$(dir_results_to_calibrate)/results/_result.hdf5"] #adjust filename, can add multiple files
 
 exportTraces = true # if true, check HeaderForExportDict below:
 HeaderForExportDict = Dict(
@@ -45,15 +68,22 @@ HeaderForExportDict = Dict(
         "version"=>"01",
         "authorname_mail"=>"Ruth, Clea clea.ruth@student.uibk.ac.at",
         "units"=>"ppt",
-        "addcomment"=>"The data have been humidity-depently calibrated with Hexanone as reference (Onr=1),
-        compounds with Onr>1 are calibrated with kinetic limit.
-        All traces have been corrected to the duty-cycle-corrected primary ion trace.
-        Uncertainty roughly factor 3. Not transmission-corrected yet.\n",
+        "addcomment"=>"The data have been humidity-depently calibrated with Hexanone as reference (Onr=1), compounds with Onr>1 are calibrated with kinetic limit. All traces have been corrected to the duty-cycle-corrected primary ion trace. Uncertainty roughly factor 3. Not transmission-corrected yet.\n",
         "threshold"=>0,
         "nrrows_addcomment" => 4
         )
 
+#= humparams is not defined yet
+humparams=(Float64[],Float64[]," ")
+humparams = fitParameters(cloudhumfinal, licorfinal; functiontype="exponential")
+cloudhumfinal = IntpF.sortSelectAverageSmoothInterpolate(time2interpolate2, cloudhum.time, cloudhum[!,cloudhumLabel]; returnSTdev=false, selectY=selectY.cloud)
+licorfinal = IntpF.sortSelectAverageSmoothInterpolate(time2interpolate2, licorDat.datetime, licorDat.H2O_mmolpermol; returnSTdev=false, selectY=selectY.inlet)
+cloudhum = CSV.read(frostpointfile, DataFrame; dateformat=frostpointDatetimeFormat)
+    if Year(cloudhum.time[1]) < Year(999)
+        cloudhum.time = cloudhum.time .+ Year(2000)
+    end =#
 
+# 
 #####################################################
 # load and prepare metadata of the final calibration
 #####################################################
@@ -89,10 +119,6 @@ function load_hexVSpis_params(drycalibsfile::String)
 end
 
 
-if !isdefined(Main,:hexVSpis_params)
-    hexVSpis_params = load_hexVSpis_params(drycalibsfile)
-end
-
 #load humidity data inlet
 function load_licor_data(dir_licor_data::String)
     return ImpF.createLicorData_fromFiles(dir_licor_data;
@@ -102,9 +128,6 @@ function load_licor_data(dir_licor_data::String)
         type_columnOfInterest=Float64)
 end
 
-if !isdefined(Main,:licorDat)
-    licorDat = load_licor_data(dir_licor_data)
-end
 
 #interpolate licor data to ptr time
 function interpolate_licor_to_ptr_time(mResfinal, licorDat)
@@ -114,21 +137,22 @@ function interpolate_licor_to_ptr_time(mResfinal, licorDat)
             licorDat.time,
             licorDat.value;
             returnSTdev = false,
-            selectY = [0.0, Inf]
+            selectY = [0.0, Inf] #[-21, 5]
         )
     return licor_final
 end
 
+
 #plot humidity dependent calibration
-function plot_humidity_dependent_calibration(humcalibfp, humparams, licorDat, ionization)
-    calibDF = CSV.read(humcalibfp, DataFrame; header=2)
+function plot_humidity_dependent_calibration(humcalibfile, humparams, licorDat, ionization)
+    calibDF = CSV.read(humcalibfile, DataFrame; header=2)
 
     CalF.plot_humdep_fromCalibParameters(
         calibDF = calibDF,
         humparams = humparams,
-        cloudhum = licorDat.value,   # uses now Licor directly
+        cloudhum = licorDat.value,   # uses now Licor directly #TO DO check if ok!!!
         hum4plot = collect(0:0.2:12),
-        savefp = dirname(humcalibfp),
+        savefp = dirname(humcalibfile),
         humdepcalibRelationship = "double exponential",
         humidityRelationship = "exponential",
         ionization = ionization
@@ -195,35 +219,27 @@ function build_calibration_traces(
     hexVSpis_params,
     refName)
 
-    licor_final =
-        IntpF.sortSelectAverageSmoothInterpolate(
-            mResfinal.Times,
-            licorDat.time,
-            licorDat.value;
-            returnSTdev = false,
-            selectY = [-21, 5]
-        )
-
     #initialize calibration traces
-    dcps_per_ppb =
-        zeros(length(mResfinal.Times), length(mResfinal.MasslistMasses))
+    dcps_per_ppb = zeros(length(mResfinal.Times), length(mResfinal.MasslistMasses))
 
     fref = findfirst(calibDF[!, "Sumformula"] .== refName)
     refparams = calibDF[fref, [:p1, :p2, :p3, :p4, :p5]]
 
-    undeffilter =
-        vec(sum(mResfinal.MasslistCompositions; dims=1)) .== 0
-    greater2oxygenfilter =
-        vec(mResfinal.MasslistCompositions[
-            mResfinal.MasslistElements .== "O", :] .>= 2)
+    #composition based filters
+    undeffilter = BitVector(sum(mResfinal.MasslistCompositions; dims=1)[1, :] .== 0)
+
+    oxygen_number = findfirst(==("O"), mResfinal.MasslistElements)
+
+    oneoxygenfilter = BitVector(mResfinal.MasslistCompositions[oxygen_number, :] .== 1)
+
+    twoplusoxygenfilter = BitVector(mResfinal.MasslistCompositions[oxygen_number, :] .>= 2)
+    
+    #hexanone-primary-ion factor
+    f_hex = CalF.applyFunction(summedPIs, hexVSpis_params[1]; functiontype = hexVSpis_params[3][1])
 
     #dry / kinetic limit calibration for undef and >=2 O
-    dcps_per_ppb[:, (undeffilter .| greater2oxygenfilter)] .=
-        CalF.applyFunction(
-            summedPIs,
-            hexVSpis_params[1];
-            functiontype = hexVSpis_params[3][1]
-        ) .*
+    dcps_per_ppb[:, (undeffilter .| twoplusoxygenfilter)] .=
+        f_hex .*
         CalF.applyFunction(
             zeros(length(mResfinal.Times)),
             refparams;
@@ -231,15 +247,8 @@ function build_calibration_traces(
         )
 
     #humid / equilibrium calibration for 1 O
-    dcps_per_ppb[:,
-        vec(mResfinal.MasslistCompositions[
-            mResfinal.MasslistElements .== "O", :] .== 1)
-    ] .=
-        CalF.applyFunction(
-            summedPIs,
-            hexVSpis_params[1];
-            functiontype = hexVSpis_params[3][1]
-        ) .*
+    dcps_per_ppb[:, oneoxygenfilter] .=
+        f_hex .*
         CalF.applyFunction(
             CalF.applyFunction(
                 licor_final,
@@ -252,7 +261,7 @@ function build_calibration_traces(
 
     indices = Int[]
 
-    for (name, mass) in zip(calibDF[!, "Sumformula"], calibDF[!, "Mass"])
+ #= for (name, mass) in zip(calibDF[!, "Sumformula"], calibDF[!, "Mass"])
         params =
             calibDF[
                 findfirst(calibDF[!, "Sumformula"] .== name),
@@ -263,14 +272,15 @@ function build_calibration_traces(
             findfirst(
                 isapprox.(mResfinal.MasslistMasses, mass, atol=0.0001)
             )
+ =#
+    for row in eachrow(calibDF)
+    params = row[[:p1, :p2, :p3, :p4, :p5]]
+
+    index = findfirst(isapprox.(mResfinal.MasslistMasses, row.Mass; atol=1e-4))
 
         if index isa Int
             dcps_per_ppb[:, index] =
-                CalF.applyFunction(
-                    summedPIs,
-                    hexVSpis_params[1];
-                    functiontype = hexVSpis_params[3][1]
-                ) .*
+                f_hex .*
                 CalF.applyFunction(
                     CalF.applyFunction(
                         licor_final,
@@ -379,14 +389,23 @@ end
 # Main Script
 ################################
 
-function main(config::CalibrationConfig)
-    hexVSpis_params = load_hexVSpis_params(config.drycalibsfile)
 
-    licorDat = load_licor_data(config.licorFilepath)
+
+function calibrate_traces_main(config::CalibrationConfig)
+
+    hexVSpis_params =
+        config.hexVSpis_params === nothing ?
+            load_hexVSpis_params(config.drycalibsfile) :
+            config.hexVSpis_params
+
+    licorDat =
+        config.licorDat === nothing ?
+            load_licor_data(config.dir_licor_data) :
+            config.licorDat
 
     calibDF =
         plot_humidity_dependent_calibration(
-            config.humcalibfp,
+            config.humcalibfile,
             humparams,
             licorDat,
             config.ionization
