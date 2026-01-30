0af014ff791c29602b9124fcba747a76497f0ae9
Timo Wittler <wittler.timo@uibk.ac.at>
2026-01-30 15:04:35 +0100

    minor bug fixes

--- src/CLOUD18_calibration.jl
+++ src/CLOUD18_calibration.jl
@@ -4,6 +4,10 @@ A Julia package for calibrating CLOUD18 data.
 """
 module CLOUD18_calibration
 
+include("humidity_dependence_calibration.jl")
+using .HumidityDependenceCalibration: run_humidity_dependence_calibration
+export HumidityDependenceCalibration, run_humidity_dependence_calibration
+
 include("calibrate_traces.jl")
 using .CalibrateTraces: calibrate_traces_main
 export CalibrateTraces, calibrate_traces_main
